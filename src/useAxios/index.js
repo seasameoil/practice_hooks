6ce4dc3b4d0fe19934a293f3c8c40b7de653b018
seasameoil <whdwlgus12@gmail.com>
2021-09-25 21:06:11 +0900

    2021.09.25

---
+++ src/useAxios/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import useAxios from "./useAxios";
+
+const App = () => {
+  const { loading, data, error, refetch } = useAxios({
+    url: "https://yts-proxy.now.sh/list_movies.json",
+  });
+  return (
+    <div className="App">
+      <h1>{data && data.status}</h1>
+      <h2>{loading && "Loading..."}</h2>
+      <button onClick={refetch}>Refetch</button>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
